01a1c3aec082c85ca91099219e45e589b879c4c5
Zhang Wei <weiz@lambdacloud.com>
2015-04-15 15:28:49 +0800

    Add elastic2json script.
    Changed the elastic-pipe to objectMode which can be piped to others,
    such as jsonline, or csv stringify.
    
    Also added mocha test cases.
    
    Signed-off-by: Zhang Wei <weiz@lambdacloud.com>

--- lambda-util.js
+++ lambda-util.js
@@ -1,1 +1,5 @@
+exports.Csv = require('./lib/csv-trans');
 exports.ElasticPipe = require('./lib/elastic/elastic-pipe');
+exports.ElasticDocFlat = require('./lib/elastic/elastic-docflat-trans');
+exports.JsonLine = require('./lib/jsonline-trans');
+exports.SetsPlus = require('./lib/sets-plus');
