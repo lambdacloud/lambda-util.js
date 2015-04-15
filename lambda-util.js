92aeb7e40fc998e20224b1437fff71241d880bbf
Zhang Wei <weiz@lambdacloud.com>
2015-04-15 15:32:44 +0800

    Remove the non-exist csv-trans requires.
    Signed-off-by: Zhang Wei <weiz@lambdacloud.com>

--- lambda-util.js
+++ lambda-util.js
@@ -1,4 +1,3 @@
-exports.Csv = require('./lib/csv-trans');
 exports.ElasticPipe = require('./lib/elastic/elastic-pipe');
 exports.ElasticDocFlat = require('./lib/elastic/elastic-docflat-trans');
 exports.JsonLine = require('./lib/jsonline-trans');
