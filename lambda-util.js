d2e77ad221ce0d23da330d7f33bb6ed92da347a7
sunyonggang <sun.yonggang@lambdacloud.com>
2015-04-29 18:28:06 +0800

    load json to elasticsearch

--- lambda-util.js
+++ lambda-util.js
@@ -3,3 +3,5 @@ exports.ElasticPipe = require('./lib/elastic/elastic-pipe');
 exports.ElasticDocFlat = require('./lib/elastic/elastic-docflat-trans');
 exports.JsonLine = require('./lib/jsonline-trans');
 exports.SetsPlus = require('./lib/sets-plus');
+exports.StringLine = require("./lib/stringline-trans");
+exports.ElasticNewPipe = require('./lib/elastic/elasticwrite-pipe');
