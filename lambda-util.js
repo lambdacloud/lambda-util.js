bd5ca65652a72bce627546fc86124b46e2bb1668
sunyonggang <sun.yonggang@lambdacloud.com>
2015-05-19 15:05:37 +0800

    load data to elasticsearch with one message

--- lambda-util.js
+++ lambda-util.js
@@ -1,8 +1,8 @@
 exports.ArrayTrans = require('./lib/array-trans');
-exports.ElasticReadPipe = require('./lib/elastic/elastic-read-pipe');
+exports.ElasticReadPipe = require('./lib/elastic/elastic-pipe');
 exports.ElasticDocFlat = require('./lib/elastic/elastic-docflat-trans');
 exports.JsonLine = require('./lib/jsonline-trans');
 exports.SetsPlus = require('./lib/sets-plus');
 exports.StringLine = require("./lib/elastic/elastic-indexparam-trans");
 exports.ElasticWritePipe = require("./lib/elastic/elastic-write-pipe");
-exports.StringArray = require("./lib/elastic/elastic-arraysparam-trans");
+//exports.StringArray = require("./lib/elastic/elastic-arraysparam-trans");
