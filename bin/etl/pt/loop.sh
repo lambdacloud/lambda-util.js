035a77653664b2258e348b5e7242d12edcca16b7
sunyonggang <newbegin2020@163.com>
2015-12-01 13:44:15 +0800

    add comments & remove log info

--- bin/etl/pt/loop.sh
+++ bin/etl/pt/loop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for f in *.csv ; do
-  node ../../bin/etl/pt/pt2lambdacloud -H 'api.lambdacloud.com' --file "$f" --token '2E83B154-33ED-4087-836E-AC77A88C7BF8' --type "${f%.csv}_db"
+  node ../../bin/etl/pt/pt2lambdacloud -H 'api.lambdacloud.com' --file "$f" --token 'pt_token' --type "${f%.csv}_db"
 done
